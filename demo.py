1a27eda50564684dcd8773a13c82280a10f56c19
Sahil Shaikh <sahilshaikh8264@gmail.com>
2024-12-24 04:12:54 +0000

    Added comment for better readability

--- demo.py
+++ demo.py
@@ -1,7 +1,13 @@
+#A small program to check product price
+
+#Declared variable which contains product price
 ferrari = 42000
 toyota = 21000
 
+#Checking if product A is greater then proudct B or not
 if ferrari > toyota:
     print("ferrari is expensive then toyota")
-else:
+elif toyota > ferrari:
     print("Toyota is expensive then ferrari")
+else:
+    print("Both has same price")
