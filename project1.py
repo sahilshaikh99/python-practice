bdadedb7e50874e917bffa587ef11f1031d3016e
Sahil Shaikh <sahilshaikh8264@gmail.com>
2024-12-24 05:07:07 +0000

    Added Personal Information Card project

---
+++ project1.py
@@ -0,0 +1,12 @@
+#Project: Personal Information Card
+#Create a Python script that displays a personal information card with the following details: Name, Age, City, Hobby
+name = "Sahil Shaikh"
+age = 26
+city = "Leeds, UK"
+hobby = "Tech, GYM, Reading"
+
+print("Personal Information:")
+print("Name: " +name)
+print("Age: ", age)
+print("City: "+city)
+print("Hobby: "+hobby)
