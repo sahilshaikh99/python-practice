9a03a9837255ae42bf6ff18b30dc87d28d0c7968
Sahil Shaikh <sahilshaikh8264@gmail.com>
2024-12-24 04:49:31 +0000

    Added type casting excercise

---
+++ casting.py
@@ -0,0 +1,15 @@
+#This is file which contains some basic casting excercise
+
+x = 10
+y = "hello"
+
+#checking variable type
+print(type(x))
+print(type(y))
+
+#type casting of variable
+a = str("hii")
+b = int(20)
+c = float(5)
+
+print(a, b, c)
